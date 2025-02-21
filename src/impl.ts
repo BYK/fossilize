7a291d814f6639efd4fc0c1ac4d727e7fba99f6f
Burak Yigit Kaya <byk@sentry.io>
2025-02-21 15:26:42 +0000

    fix: Actually fail and exit when `run` fails
    When a file is not found with the `run` helper, the error won't have `.code` on it, causing us to exit the process without an error code. This patch fixes it.

--- src/impl.ts
+++ src/impl.ts
@@ -49,7 +49,11 @@ async function run(cmd: string, ...args: string[]): Promise<string> {
     console.error(`Failed to \`run ${cmd} ${args.join(" ")}\``);
     console.error((err as ExecError).stdout);
     console.error((err as ExecError).stderr);
-    process.exit((err as ExecError).code);
+    if ((err as ExecError).code) {
+      process.exit((err as ExecError).code);
+    } else {
+      throw new Error("Bailing out as the command failed");
+    }
   }
   if (output.stdout.trim()) {
     console.log(output.stdout);
